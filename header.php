f91ea279a432ef1dff9df63aabd2b2c4886dc383
Erfan Asadi <asadi.love7981@gmail.com>
2023-06-13 18:10:17 +0330

    complete header component

--- header.php
+++ header.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <header>
-        <div class="flex">
-          <a href="" class="logo">ایفود </a>
-          <nav class="navbar">
-            <ul>
-                 <li>مشاهده محصولات</li>
-                 <li>فروشگاه</li>
-            </ul>
-          </nav>
-          <a href="cart.php"><i class="bi bi-cart-check-fill"></i></a>
-        </div>
+        
+            <a href="" class="logo">ایفود </a>
+            <nav class="navbar">
+                <ul>
+                    <li> 
+                        <a href="admin.php">مشاهده محصولات</a>
+                    </li>
+                    <li> 
+                        <a href="products.php">فروشگاه</a>
+                    </li>
+                </ul>
+            </nav>
+            <a href="cart.php" class="cart">
+                <i class="bi bi-cart-check-fill"></i>
+                <span>0</span>
+            </a>
+        
     </header>
 </body>
+
 </html>
