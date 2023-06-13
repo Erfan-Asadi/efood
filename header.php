7099055d4cd064aeb06f2f67c39dba4dfbd5e4ef
Erfan Asadi <asadi.love7981@gmail.com>
2023-06-13 17:45:23 +0330

    initialize project, set database and create some pages

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="flex">
+          <a href="" class="logo">ایفود </a>
+          <nav class="navbar">
+            <ul>
+                 <li>مشاهده محصولات</li>
+                 <li>فروشگاه</li>
+            </ul>
+          </nav>
+          <a href="cart.php"><i class="bi bi-cart-check-fill"></i></a>
+        </div>
+    </header>
+</body>
+</html>
