6d6667926c7433b8877b6377a1ffa7a459d854c7
Erfan Asadi <asadi.love7981@gmail.com>
2023-06-14 01:53:19 +0330

    can add new product, complete show products table view

--- admin.php
+++ admin.php
@@ -1,12 +1,80 @@
+<?php
+include 'connection.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css">
     <title>پنل ادمین</title>
 </head>
+
 <body>
-    
+    <?php include 'header.php'; ?>
+    <?php
+    if (isset($message)) {
+        foreach ($message as $message) {
+            echo '
+                    <div class="message">
+                        <span> ' . $message . ' <i class="bi bi-x"
+                                    onclick="this.parentElement.style.display=`none`"></i>
+                        </span>
+                    </div>
+                ';
+        }
+    }
+    ?>
+    <a href="product_form.php" class="add">+</a>
+    <section class="show-product">
+        <table class="product-table">
+            <thead>
+                <tr>
+                    <th>عکس</th>
+                    <th>نام</th>
+                    <th>قیمت</th>
+                    <th colspan="2">اکشن</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+                if (mysqli_num_rows($select_products) > 0) {
+                    while ($row = mysqli_fetch_assoc($select_products)) {
+
+                        ?>
+                        <tr>
+                            <td><img src="images/<?php echo $row['image']; ?>"></td>
+                            <td>
+                                <?php echo $row['name']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['price']; ?>ت
+                            </td>
+                            <td>
+                                <a title="حذف" href="admin.php?delete=<?php echo $row['id']; ?>" class="delete-btn"
+                                    onclick="return confirm('از حذف محصول اطمینان دارید؟')">
+                                    <i class="bi bi-trash"></i>
+                                    حذف
+                                </a>
+                            </td>
+                            <td>
+                                <a title="ویرایش" href="admin.php?edit=<?php echo $row['id']; ?>" class="option-btn">
+                                    <i class="bi bi-pencil"></i>
+                                    ویرایش
+                                </a>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                }
+                ?>
+            </tbody>
+        </table>
+    </section>
 </body>
+
 </html>
--- product_form.php
+++ product_form.php
@@ -1,3 +1,26 @@
+<?php 
+    include 'connection.php';
+
+    if(isset($_POST['add_product'])) {
+        $name = $_POST['p_name'];
+        $price = $_POST['p_price'];
+        $p_image = $_FILES['p_image']['name'];
+        $p_image_temp_name = $_FILES['p_image']['tmp_name'];
+        $p_image_folder = 'image/'.$p_image;
+
+        $query = "INSERT INTO `products`(`name`, `price`, `image`) VALUES ('$name','$price', '$p_image')";
+        $insert_query = mysqli_query($conn, $query);
+
+        if($insert_query) {
+            move_uploaded_file($p_image_temp_name, $p_image_folder);
+            $message[] = 'محصول با موفقیت افزوده شد.';
+            header('location:admin.php');
+        } else {
+            $message[] = 'افزودن محصول با خطا مواجه شد.';
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,5 +33,26 @@
 </head>
 <body>
     <?php include 'header.php'; ?>
+    <?php 
+        if(isset($message)) {
+            foreach ($message as $message) {
+                echo '
+                    <div class="message">
+                        <span> '.$message.' <i class="bi bi-x"
+                                    onclick="this.parentElement.style.display=`none`"></i>
+                        </span>
+                    </div>
+                ';
+            }
+        }
+    ?>
+    <div class="form">
+        <form method="post" enctype="multipart/form-data">
+            <h3>افزودن محصول جدید </h3>
+            <input type="text" name="p_name" placeholder="نام محصول" required>
+            <input type="number" name="p_price" min="0" placeholder="قیمت محصول" required>
+            <input type="file" name="p_image" accept="image/png, image/jpg, image/jpeg" required>
+            <input type="submit" name="add_product" value="افزودن" class="btn submit-button">
+        </form>
 </body>
 </html>
